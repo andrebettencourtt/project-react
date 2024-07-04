2df56df42a0d23a35779f113dd39d14eced495ee
andrebettencourtt <andreluispb10@gmail.com>
2024-07-03 21:29:59 -0300

    feat: creating side

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import "./global.css";
+import styles from "./App.module.css";
 
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <h1>Conteúdo</h1>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
